cee6f49fcc2ee3385898ee67123ec264fd4f95a1
Arthur Dias <arthursantos01@gmail.com>
2023-06-21 12:18:05 -0300

    added cors + ratelimiter

--- src/app.ts
+++ src/app.ts
@@ -8,25 +8,30 @@ import { router } from './shared/infra/http/routes';
 import swaggerFile from './swagger.json';
 import "./shared/container"
 import createConnection from "./shared/infra/typeorm/index";
+import cors from 'cors';
 import { AppError } from '@shared/errors/AppError';
+import rateLimiter from '@middlewares/rateLimiter'
 
 createConnection();
 const app = express();
+app.use(rateLimiter);
+app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 // app.use(express.static('/public'));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
-app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
+    console.log('App err ================', err)
     return response.status(err.statusCode).json({
       message: err.message
     });
   }
-  console.log('err', err)
+  console.log('App err ================', err)
   return response.status(500).json({
     status: "Error",
     message: `Internal server error ${err.message}`
---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import * as redis from "redis";
+
+import { AppError } from "@shared/errors/AppError";
+
+const redisClient = redis.createClient({
+  legacyMode: true,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+(async () => await redisClient.connect())()
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 10,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+    return next();
+  } catch (e) {
+    throw new AppError("Too many requests", 429);
+  } finally {
+    await redisClient.disconnect();
+  }
+}
